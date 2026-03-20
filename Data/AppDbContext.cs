bd67d0ba5b628d7af064008528ebba8aa7115696
Erick Chamorro Solano <eschamorro97@hotmail.com>
2026-03-20 14:06:22 -0500

    + Preparando conexion a DB, Creacion de Context y Servicios.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using CannonPackingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CannonPackingAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Towel> Towels => Set<Towel>();
+        public DbSet<Box> Boxes => Set<Box>();
+        public DbSet<BoxTowel> BoxTowels => Set<BoxTowel>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Towel>()
+                .HasIndex(u => u.ItemCode)
+                .IsUnique();
+
+            modelBuilder.Entity<Box>()
+                .HasIndex(b => b.BoxCode)
+                .IsUnique();
+
+            modelBuilder.Entity<BoxTowel>()
+                .HasOne(bi => bi.Box)
+                .WithMany(b => b.BoxTowels)
+                .HasForeignKey(bi => bi.BoxId);
+
+            modelBuilder.Entity<BoxTowel>()
+                .HasOne(bi => bi.towel)
+                .WithMany(u => u.BoxTowels)
+                .HasForeignKey(bi => bi.TowelId);
+
+            modelBuilder.Entity<BoxTowel>()
+                .HasIndex(bi => bi.TowelId)
+                .HasFilter("IsActive = 1")
+                .IsUnique();
+        }
+    }
+}
---
+++ Services/BoxService.cs
@@ -0,0 +1,47 @@
+﻿using CannonPackingAPI.Data;
+using CannonPackingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CannonPackingAPI.Services
+{
+    public class BoxService
+    {
+        private readonly AppDbContext _context;
+
+        public BoxService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddTowelToBox(int boxId, int towelId)
+        {
+            var box = await _context.Boxes.FindAsync(boxId);
+            if (box == null || !box.IsActive)
+                throw new Exception("La caja no existe.");
+
+            if (box.BoxStatus != "OPEN")
+                throw new Exception("La caja está cerrada.");
+
+            var currentCount = await _context.BoxTowels
+                .CountAsync(x => x.BoxId == boxId && x.IsActive);
+
+            if (currentCount >= box.Capacity)
+                throw new Exception("La caja está al limite de capacidad.");
+
+            var unitInAnotherBox = await _context.BoxTowels
+                .AnyAsync(x => x.TowelId == towelId && x.IsActive);
+
+            if (unitInAnotherBox)
+                throw new Exception("La unidad ya está en otra caja.");
+
+            var boxItem = new BoxTowel
+            {
+                BoxId = boxId,
+                TowelId = towelId
+            };
+
+            _context.BoxTowels.Add(boxItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
