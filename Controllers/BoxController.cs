5fc29b9ff9280053577fcea168bb06869551ef49
Erick Chamorro Solano <eschamorro97@hotmail.com>
2026-03-20 14:12:56 -0500

    + Controladores.

---
+++ Controllers/BoxController.cs
@@ -0,0 +1,36 @@
+﻿using CannonPackingAPI.Data;
+using CannonPackingAPI.Models;
+using CannonPackingAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CannonPackingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BoxController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly BoxService _service;
+
+        public BoxController(AppDbContext context, BoxService service)
+        {
+            _context = context;
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Box box)
+        {
+            _context.Boxes.Add(box);
+            await _context.SaveChangesAsync();
+            return Ok(box);
+        }
+
+        [HttpPost("{boxId}/add-unit/{unitId}")]
+        public async Task<IActionResult> AddUnit(int boxId, int unitId)
+        {
+            await _service.AddTowelToBox(boxId, unitId);
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/TowelController.cs
@@ -0,0 +1,37 @@
+﻿using CannonPackingAPI.Data;
+using CannonPackingAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CannonPackingAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TowelController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TowelController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Towel towel)
+        {
+            _context.Towels.Add(towel);
+            await _context.SaveChangesAsync();
+            return Ok(towel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var units = await _context.Towels
+                .Where(x => x.IsActive)
+                .ToListAsync();
+
+            return Ok(units);
+        }
+    }
+}
