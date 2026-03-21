6f4ad58739e6acc044e907715acc9f0fe6553fb2
Erick Chamorro Solano <eschamorro97@hotmail.com>
2026-03-20 22:02:11 -0500

    + configuracion para ejecutarlo en navegador y que este se abra.

--- Program.cs
+++ Program.cs
@@ -12,12 +12,19 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CannonPackingAPI");
+        c.RoutePrefix = string.Empty; // <--- esto hace que Swagger abra en /
+    });
+}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
